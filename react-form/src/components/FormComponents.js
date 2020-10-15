abd1bd0ccec6d02e38feeb5792c86e8b90e7cd33
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-10-15 22:42:15 +0500

    try to add form validation

--- react-form/src/components/FormComponents.js
+++ react-form/src/components/FormComponents.js
@@ -13,6 +13,8 @@ class FromComponent extends Component {
             email: '',
             age: '',
             about: '',
+
+            errors: ""
         }
     }
 
@@ -28,9 +30,23 @@ class FromComponent extends Component {
         const { name, value } = event.target
         this.setState({
             [name]: value
-        })
-    }
+        });
 
+        let fname = event.target.value;
+
+        if (fname == "") {
+            this.setState({ errors: "Invalid" })
+        }
+        this.setState({
+            [name]: value
+        });
+
+
+        // this.setState({
+        //     errors, [name]: value
+        // })
+        console.log(this.state)
+    }
 
     // reset form function
     clearStates = () => {
@@ -45,11 +61,12 @@ class FromComponent extends Component {
 
     // showing form data
     formSubmission = (e) => {
+
         swal(
             "Good job!",
             `Name: ${this.state.firstName} ${this.state.lastName}
-            Email: ${this.state.email}, Age: ${this.state.age}
-            About: ${this.state.about}`,
+                Email: ${this.state.email}, Age: ${this.state.age}
+                About: ${this.state.about}`,
             "success"
         );
         e.preventDefault();
@@ -59,30 +76,31 @@ class FromComponent extends Component {
 
     render() {
         // const disabledCondition = this.state.firstName === '' || this.state.lastName === '' || this.state.age === '' || this.state.email === '' || this.state.about === ''
-
+        const { errors } = this.state
         return (
             <div className="container">
                 <form onSubmit={this.formSubmission}>
                     <h1 className="App mt-3">React Form Handling</h1>
                     <div className="row mt-5">
                         <div className="col-md-6">
-                            <input type="text" name="firstName" required onChange={this.StateChangeHandler} placeholder="First Name e.g Aman" value={this.state.value} className="form-control" />
+                            <input type="text" name="firstName" onChange={this.StateChangeHandler} placeholder="First Name e.g Aman" value={this.state.value} className="form-control" />
+                            <small style={{ color: "red" }}>{errors.username}</small>
                         </div>
                         <div className="col-md-6">
-                            <input type="text" name="lastName" required onChange={this.StateChangeHandler} placeholder="Last Name e.g Sultan" value={this.state.value} className="form-control" />
+                            <input type="text" name="lastName" onChange={this.StateChangeHandler} placeholder="Last Name e.g Sultan" value={this.state.value} className="form-control" />
                         </div>
                     </div>
                     <div className="row mt-4">
                         <div className="col-md-6">
-                            <input type="email" name="email" required onChange={this.StateChangeHandler} placeholder="Email e.g Johndeo@gmail.com" value={this.state.value} className="form-control" />
+                            <input type="email" name="email" onChange={this.StateChangeHandler} placeholder="Email e.g Johndeo@gmail.com" value={this.state.value} className="form-control" />
                         </div>
                         <div className="col-md-6">
-                            <input type="number" name="age" required onChange={this.StateChangeHandler} min="0" placeholder="Age e.g 25" value={this.state.value} className="form-control" />
+                            <input type="number" name="age" onChange={this.StateChangeHandler} min="0" placeholder="Age e.g 25" value={this.state.value} className="form-control" />
                         </div>
                     </div>
                     <div className="row mt-3">
                         <div className="col-md-12">
-                            <textarea rows="4" name="about" required onChange={this.StateChangeHandler} placeholder="About Yourself" value={this.state.value} className="form-control"></textarea>
+                            <textarea rows="4" name="about" onChange={this.StateChangeHandler} placeholder="About Yourself" value={this.state.value} className="form-control"></textarea>
                         </div>
                     </div>
                     <button className="btn btn-primary submit-btn float-right mt-3">Submit Form</button>
